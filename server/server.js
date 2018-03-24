b5a4b3a4749d4558a69fed116813204596664460
James Kamana <jaykamana@gmail.com>
2018-03-24 11:15:10 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 let express = require('express');
 var bodyParser = require('body-parser');
 
@@ -28,6 +30,22 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }). catch((err) => res.status(400).send());
 })
 
 app.listen(3000, () => {
