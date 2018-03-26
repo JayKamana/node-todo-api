b5976eea75f8ee76947ca80a8a5a243c029ad11c
James Kamana <jaykamana@gmail.com>
2018-03-26 18:38:04 +0300

    Add test cases for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -211,7 +211,7 @@ describe('POST /users', () => {
         expect(user).toExist();
         expect(user.password).toNotBe(password)
         done();
-      })
+      }).catch((err) => done(err));
     })
   })
 
@@ -235,5 +235,54 @@ describe('POST /users', () => {
     .send({email, password})
     .expect(400)
     .end(done)
+  })
+})
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        })
+        done();
+      }).catch((err) => done(err));
+    })
+  })
+
+  it('should reject invalid login', (done) => {
+    let email = users[1].email
+    let password = 'xyz'
+
+    request(app)
+    .post('/users/login')
+    .send({email, password})
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      User.findOne({email}).then((user) => {
+        expect(user.tokens).toEqual([]);
+        done()
+      }).catch((err) => done(err))
+    })
   })
 })
